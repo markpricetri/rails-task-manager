9485d347c1ef95b5a84065acc82091906cc08b20
Mark Price <markempower94@gmail.com>
2021-02-16 13:22:29 +0000

    create read update delete methods

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.
   get '/tasks', to: 'tasks#index'
+
+  get 'tasks/new', to: 'tasks#new', as: 'new'
+  post 'tasks', to: "tasks#create"
+
+  get 'tasks/:id/edit', to: 'tasks#edit', as: 'edit'
+  patch 'tasks/:id', to: 'tasks#update'
+
+  get '/tasks/:id', to: 'tasks#show', as: 'task'
 end
